ec000a344d1e994610dfb97d142e57736764525d
WhereIsWoldemort <morisete@uni.edu>
2016-11-11 20:28:17 -0600

    removed unneccessary import statements from Main.java

--- sources/Main.java
+++ sources/Main.java
@@ -9,19 +9,12 @@ import java.util.List;
 import java.util.Map;
 import java.util.HashMap;
 
-
 import org.apache.http.HttpEntity;
-import org.apache.http.NameValuePair;
-import org.apache.http.client.entity.UrlEncodedFormEntity;
 import org.apache.http.client.methods.CloseableHttpResponse;
 import org.apache.http.client.methods.HttpGet;
-import org.apache.http.client.methods.HttpPost;
 import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.HttpClients;
-import org.apache.http.message.BasicNameValuePair;
 import org.apache.http.util.EntityUtils;
-
-
 
 public class Main {
 	final static String URL = "https://banno.com/";
